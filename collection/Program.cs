7d47f49d07658073b8f35863bf7244678d346e3b
indira <indira.sultanova.2002@mail.ru>
2020-06-15 15:06:30 +0500

    commit collection

---
+++ collection/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+
+namespace collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList List = new ArrayList() { 1, 2,3,4,5,-6,-7,-8,-9,-10 };
+            List.Add(5.8);
+            foreach (object o in List)
+            {
+                Console.WriteLine(o);
+            }
+            Console.WriteLine("число элементов коллекции: {0}", List.Count);
+        }
+    }
+}
